90c577b0d76244c41e3f1b46343fd3bad602aa5a
ASM-Monirul-Islam <monirul.01401411046@gmail.com>
2025-07-20 16:28:33 +0600

    Updated login procedure

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ const menuCard = document.querySelector(".menu-bg");
 menu.addEventListener("click", ()=>{
 	menuCard.classList.toggle("menu-card-hidden")
 })
+
+const navLogin = document.querySelector("#nav-login");
+const menuLogin = document.querySelector(".menu-Login");
+const loginPop = document.querySelector(".login-popup");
+
+// navLogin.addEventListener("click", ()=> {
+// 	loginPop.classList.toggle("login-panel-hidden");
+// })
+
+// menuLogin.addEventListener("click", ()=> {
+// 	loginPop.classList.toggle("login-panel-hidden");
+// })
+	
+let login = {
+	l1 : document.querySelector("#nav-login"),
+	l2 : document.querySelector(".menu-Login")
+}
+
+for(const key in login) {
+	login[key].addEventListener("click", ()=>{
+		loginPop.classList.toggle("login-panel-hidden");
+	})
+}
